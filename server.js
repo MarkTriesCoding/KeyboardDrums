d7fb84ad67587d3489f77162eea702fc6c4ef7c6
Mark Halstead <mark.d.halstead@gmail.com>
2020-05-01 12:27:43 -0600

    Working server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000  ;
 // const sass = require('node-sass');
 const fs = require('fs');
 const path = require('path');
-app.use(express.static(__dirname+'/src'));
+app.use(express.static(__dirname+'/dist'));
 
 app.use(cors())
 
